03535abea130847c36a02fc3099a5e1c6d5cf920
philippe gabriel <philippe.gabriel@citrix.com>
2014-05-30 18:02:34 +0100

    add example of multi-join query to get components

---
+++ CA.inflow.byNames.allReleases+components.sql
@@ -0,0 +1,25 @@
+-- list jira issues for the a given project
+-- Using the jira SQL interface, see: https://developer.atlassian.com/display/JIRADEV/Database+Schema
+-- Using OUTER JOIN, see: https://confluence.atlassian.com/display/JIRA041/Example+SQL+queries+for+JIRA
+-- philippeg 30may2014
+--
+select to_char(j.created, 'YYYY'),to_char(j.created, 'WW'),concat(p.pkey,'-',j.issuenum),pri.pname,j.assignee,pv.vname,c.cname
+from  jiraissue j 
+LEFT OUTER JOIN project p ON 
+	p.pkey='CA' 
+LEFT OUTER JOIN nodeassociation naver ON 
+	naver.ASSOCIATION_TYPE='IssueVersion'
+	and naver.SOURCE_NODE_ID=j.id
+LEFT OUTER JOIN projectversion pv ON
+	pv.id=naver.SINK_NODE_ID
+LEFT OUTER JOIN priority pri ON
+	pri.id=j.priority
+LEFT OUTER JOIN nodeassociation nacomp ON 
+	nacomp.ASSOCIATION_TYPE='IssueComponent'
+	and nacomp.SOURCE_NODE_ID=j.id
+LEFT OUTER JOIN component c ON 
+	c.id=nacomp.SINK_NODE_ID
+
+where j.project=p.id
+order by j.created;
+
